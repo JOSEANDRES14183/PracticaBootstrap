7117b7aef2a1705721d459f9204306c78731abb1
JOSEANDRES14183 <jreyes14183@alumnes.iesmanacor.cat>
2021-12-28 00:35:10 +0100

    terminado

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass')(require('sass'));
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
+const htmlmin = require('gulp-htmlmin');
+const uglify = require('gulp-uglify');
+const imagemin = require ('gulp-imagemin');
+const { series } = require('gulp');
 
 function buildStyles() {
   return gulp.src('./scss/**/*.scss')
@@ -15,6 +20,32 @@ function copy() {
   return gulp.src('./node_modules/bootstrap/dist/js/*')
     .pipe(gulp.dest('./js'));
 };
+
+function minifyCss(){
+  return gulp.src('css/*.css')
+      .pipe(cleanCSS({compatibility: 'ie8'}))
+      .pipe(gulp.dest('dist/css'));
+}
+
+function minifyHtml(){
+  return gulp.src('html/*.html')
+      .pipe(htmlmin({ collapseWhitespace: true }))
+      .pipe(gulp.dest('dist'));
+}
+
+function minifyJs(){
+  return gulp.src('js/*.js')
+      .pipe(uglify())
+      .pipe(gulp.dest('dist/js'));
+}
+
+function minifyImg(){
+  return gulp.src('img/*')
+      .pipe(imagemin())
+      .pipe(gulp.dest('dist/img'));
+}
+
+exports.build = series(buildStyles, copy, minifyCss, minifyHtml, minifyJs, minifyImg);
 
 exports.compilaSass = buildStyles;
 exports.watchSass = watchSass;
